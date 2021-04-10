8dc7701bffcb22349ef0438569f2a9262c5a723c
hmh <934178858@qq.com>
2021-04-10 17:34:21 +0800

    update pom.xml

--- src/main/java/com/hmh/secondskill/SecondsKillApplication.java
+++ src/main/java/com/hmh/secondskill/SecondsKillApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+//import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
